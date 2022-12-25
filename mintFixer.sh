ce8939a3a85b85d1db4cb5eb644315c8b9c6982a
jorb <jdeh,mel@outlook.com>
2022-12-25 13:21:59 -0700

    Expanded installation scripts

--- mintFixer.sh
+++ mintFixer.sh
@@ -1,18 +1,18 @@
 #!bin/bash/
 
 # Remove bloat
-sudo apt purge -y libreoffice* thingy rhythmbox thunderbird hexchat
-sudo apt purge -y openvpn redshift timeshift pidgin transmission celluloid
-sudo apt purge -y warpinator mintwelcome mintupgrade mintbackup webapps-manager
-sudo apt purge -y xfce4-dict hypnotix drawing simple-scan sticky evolution
-sudo apt purge -y onboard
+sudo apt-get purge -y libreoffice* rhythmbox thunderbird hexchat
+sudo apt-get purge -y redshift timeshift pidgin transmission-gtk celluloid
+sudo apt-get purge -y warpinator mintwelcome mintupgrade mintbackup webapp-manager
+sudo apt-get purge -y xfce4-dict hypnotix drawing simple-scan sticky evolution
+sudo apt-get purge -y onboard pix synaptic catfish
 
 # Finish removal
 sudo apt-get autopurge -y && sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 
 # Install basics
-sudo apt-get install git firefox make
+sudo apt-get install git firefox make clang
 
 # Update
 sudo apt-get update && sudo apt-get upgrade -y
