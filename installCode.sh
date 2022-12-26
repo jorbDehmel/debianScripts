9d50f932961dc9ce0481d55e37079ed5d7faadd5
jorb <jdehmel@outlook.com>
2022-12-26 10:35:58 -0700

    Made code installer actually install code

--- installCode.sh
+++ installCode.sh
@@ -1,4 +1,8 @@
-cd ~/Downloads
-wget https://codeload.github.com/jorbDehmel/debianScripts/zip/refs/heads/master
-sudo apt-get install *.deb && rm *.deb
-cd -
+#!bin/bash
+
+# Download and install vs code .deb package
+wget https://az764295.vo.msecnd.net/stable/e8a3071ea4344d9d48ef8a4df2c097372b0c5161/code_1.74.2-1671533413_amd64.deb -O code.deb
+sudo apt-get install ./code.deb && rm -f code.deb
+
+# Create my usual dir structure
+mkdir -p ~/Programs/cpp ~/Programs/scripts ~/Programs/python
