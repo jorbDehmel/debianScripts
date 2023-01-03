0a66ee810a994af2f903cc193811d6b19a289d99
jorbDehmel <jdehmel@outlook.com>
2023-01-03 13:58:35 -0700

    Added unity installer for work

---
+++ unityInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list'
+wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
+sudo apt update
+sudo apt-get install unityhub
